83f0c3ec09fbc2b0da07209cdc8cb989585c4cc0
kahoragachau <joelkahoragachau6@gmail.com>
2022-09-06 16:14:19 +0300

    - Change from using class to ID

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 function fetchWeatherData(){
-    // searchButton.addEventListener("click", ()=> alert(inputText.value))
     const city_name = document.getElementById("input-text")
     const API_key = "22fac045e2b00ac51c7d49a31bf9b146"
-    console.log(city_name.value)
-
     const searchButton = document.getElementById("button-search")
+
+
     searchButton.addEventListener("click", () => {
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_name.value}&appid=${API_key}`)
         .then(response => response.json())
-        .then(response => console.log(response))
+        .then(response => displayWeatherData(response))
         .catch(err => console.error(err));
     })
-    
-    // fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_name.value}&appid=${API_key}`)
-    //     .then(response => response.json())
-    //     .then(response => findWeatherData(response))
-    //     .catch(err => console.error(err));
 }
 
-// function findWeatherData(){
-//     const searchButton = document.getElementById("button-search")
-//     searchButton.addEventListener("click", () => console.log(data))
-// }
+function displayWeatherData(weatherData){
+    console.log(weatherData)
+    console.log(weatherData.name)
+    const displayData = document.getElementById("output-section")
+    displayData.innerHTML= weatherData.name
+}
 
 
 document.addEventListener("DOMContentLoaded", () => fetchWeatherData())
